c6ac4e8e6f0f2bb6ea837d238f3803859a8e94e5
Sofiane Fares <79220488+FaresSofiane@users.noreply.github.com>
2025-09-23 14:08:00 +0200

    ajout du service Flight avec tri

---
+++ server/service.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"sort"
+	"time"
+)
+
+type FlightService struct {
+	repositories []FlightRepository
+}
+
+func NewFlightService(repositories []FlightRepository) *FlightService {
+	return &FlightService{
+		repositories: repositories,
+	}
+}
+
+func (s *FlightService) GetAllFlights(ctx context.Context) ([]UnifiedFlight, error) {
+	var allFlights []UnifiedFlight
+	
+	for _, repo := range s.repositories {
+		flights, err := repo.GetFlights(ctx)
+		if err != nil {
+			return nil, err
+		}
+		allFlights = append(allFlights, flights...)
+	}
+	
+	return allFlights, nil
+}
+
+func (s *FlightService) GetFlightsSortedBy(ctx context.Context, sortBy string) ([]UnifiedFlight, error) {
+	flights, err := s.GetAllFlights(ctx)
+	if err != nil {
+		return nil, err
+	}
+	
+	switch sortBy {
+	case "price":
+		sort.Slice(flights, func(i, j int) bool {
+			return flights[i].Price < flights[j].Price
+		})
+	case "departure_date":
+		sort.Slice(flights, func(i, j int) bool {
+			return flights[i].DepartureTime.Before(flights[j].DepartureTime)
+		})
+	case "travel_time":
+		sort.Slice(flights, func(i, j int) bool {
+			return flights[i].TravelTime < flights[j].TravelTime
+		})
+	default:
+		sort.Slice(flights, func(i, j int) bool {
+			return flights[i].Price < flights[j].Price
+		})
+	}
+	
+	return flights, nil
+}
